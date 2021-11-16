4660187391dd8fff49a71f6b72f8a38666c5a7d4
StjepanVU <stjepan.milicic@gmail.com>
2021-11-16 05:17:38 +0100

    Modyfiyng stylesheets...

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ class Header extends react.Component {
                 <div className="row">
                     
                     <div className="col-md-6 offset-1 border header-div">
-                        {/* <span className="menu logo"><img src="https://protostar.ai/wp-content/uploads/2019/10/aple.png"/></span> */}
-                        <span className="menu"><p>Home</p></span>
-                        <span className="menu"><p>JSON</p></span>
-                        <span className="menu"><p>Contact</p></span>
+                        <span className="menu logo"><img src="https://protostar.ai/wp-content/uploads/2019/10/aple.png" width="30" height="30"/></span>
+                        <span className="menu menu-text"><p>Home</p></span>
+                        <span className="menu menu-text"><p>JSON</p></span>
+                        <span className="menu menu-text"><p>Contact</p></span>
                     </div>
                     
                     <div className="col-md-1 border bg-white search-div">
