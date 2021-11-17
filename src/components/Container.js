06071a6c9d2894440abcbb2d04726defbb8e5932
StjepanVU <stjepan.milicic@gmail.com>
2021-11-17 05:42:23 +0100

    some more styling

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,7 @@ import ContentBox from "./ContentBox";
 class Container extends react.Component {
     render() {
         return (
-            <div className="container">
+            <div className="shadow-lg p-3 mb-5 bg-body rounded container">
                 <div className="col-md-10 content-container">
                     <ContentBox/>
                 </div>    
